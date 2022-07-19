e6ee221f8ac31af3f499754712f1dbfbc613df66
Karol Moroz <k.j.moroz@gmail.com>
2022-07-20 00:55:33 +0200

    Parse all HTML files

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
+use glob::glob;
 use scraper::Html;
 use scraper::Selector;
+use std::env;
+use std::fs;
+use std::io;
+use std::path::PathBuf;
 
-fn main() {
-    let html = r#"
-    <html>
-    <body>
-    <div id="main"><h1>Content</h1></div>
-    </body>
-    </html>
-    "#;
+fn parse_file(path: &PathBuf) -> io::Result<scraper::Html> {
+    let html = fs::read_to_string(&path)?;
+    let dom = Html::parse_document(&html);
+    Ok(dom)
+}
 
-    let document = Html::parse_document(html);
-    let selector = Selector::parse("#main").unwrap();
+fn extract_main(dom: &scraper::Html) -> Option<String> {
+    let main_selector = Selector::parse("#main").unwrap();
+    dom.select(&main_selector)
+        .next()
+        .map_or(None, |elem| Some(elem.inner_html()))
+}
 
-    let element = document.select(&selector).next().unwrap();
+fn main() -> io::Result<()> {
+    let path = env::current_dir().unwrap();
+    let files_path = path
+        .parent()
+        .unwrap()
+        .join("www.bartolty.buddyzm.pl/**/*.html");
+    let files_path = files_path.to_str().unwrap();
+    let paths = glob(&files_path).unwrap();
+    for path in paths {
+        let parsed = parse_file(&path.unwrap())?;
+        if let Some(main) = extract_main(&parsed) {
+            println!("{}", main);
+        }
+    }
 
-    println!("{:?}", element.inner_html());
+    Ok(())
 }
