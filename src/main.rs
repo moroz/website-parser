9598e661b7191b87b709adc83e2ba5cad09101d0
Karol Moroz <k.j.moroz@gmail.com>
2022-07-19 23:37:18 +0200

    Find a working HTML parser, get a test working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use scraper::Html;
+use scraper::Selector;
+
 fn main() {
-    println!("Hello, world!");
+    let html = r#"
+    <html>
+    <body>
+    <div id="main"><h1>Content</h1></div>
+    </body>
+    </html>
+    "#;
+
+    let document = Html::parse_document(html);
+    let selector = Selector::parse("#main").unwrap();
+
+    let element = document.select(&selector).next().unwrap();
+
+    println!("{:?}", element.inner_html());
 }
