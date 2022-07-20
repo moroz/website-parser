2c344be145e8c2db32d5601d14de4c8cc2c2ecdf
Karol Moroz <k.j.moroz@gmail.com>
2022-07-20 02:00:44 +0200

    Remove viagra ads from pages

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,21 @@ fn extract_main(dom: &scraper::Html) -> Option<String> {
     let main_selector = Selector::parse("#main").unwrap();
     dom.select(&main_selector)
         .next()
-        .map_or(None, |elem| Some(elem.inner_html()))
+        .map_or(None, |elem| Some(elem.inner_html().trim().to_string()))
+}
+
+fn delete_viagra_ad(html: &str) -> String {
+    let mut lines: Vec<_> = html.lines().collect();
+    lines.remove(lines.len() - 1);
+    lines.join("\n")
 }
 
 fn main() -> io::Result<()> {
     let path = env::current_dir().unwrap();
+    let outpath = &path.parent().unwrap().join("out");
+    if !outpath.is_dir() {
+        fs::create_dir(&outpath)?;
+    }
     let files_path = path
         .parent()
         .unwrap()
@@ -28,9 +38,13 @@ fn main() -> io::Result<()> {
     let files_path = files_path.to_str().unwrap();
     let paths = glob(&files_path).unwrap();
     for path in paths {
-        let parsed = parse_file(&path.unwrap())?;
+        let path = path.unwrap();
+        let parsed = parse_file(&path)?;
         if let Some(main) = extract_main(&parsed) {
-            println!("{}", main);
+            let filename = path.file_name().unwrap();
+            let target = &outpath.join(filename);
+            let main = delete_viagra_ad(&main);
+            fs::write(target, main)?;
         }
     }
 
